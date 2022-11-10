76adedb4b8511ae95fcd956b19403e1403c3e515
Deepak C <deepak.c@cctsglobal.com>
2022-11-10 15:52:09 +0530

    MyFirstTestCommit: Student class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world! Thanks Deepak for testing me out");
+        Student s = new Student();
+        s.setI(2);
+        System.out.println("Test branch 1"+s.getI());
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    int i;
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public int getI() {
+        return i;
+    }
+}
