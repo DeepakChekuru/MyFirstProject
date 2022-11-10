457960ae75dfe1bf7e1e18bf1b17e7c67f15015f
Deepak C <deepak.c@cctsglobal.com>
2022-11-10 16:41:49 +0530

    MyFirstTestCommit: Car class

---
+++ src/Bike.java
@@ -0,0 +1,12 @@
+public class Bike {
+
+    String brand;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
