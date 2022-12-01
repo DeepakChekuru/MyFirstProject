7d044be4f626bc5778fd2679f72527276b7f3567
DeepakChekuru <97467545+DeepakChekuru@users.noreply.github.com>
2022-12-01 07:46:35 +0530

    Update Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,15 @@
 public class Student {
     int i;
+    
+    String department;
+    
+    public void setDepartment(String department){
+        this.department=department;
+    }
+    
+    public String getDepartment(){
+        return department;
+    }
 
     public void setI(int i) {
         this.i = i;
