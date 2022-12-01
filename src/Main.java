8430e19b86b20fc12c13fd6972e82ed777bb4a8b
DeepakChekuru <97467545+DeepakChekuru@users.noreply.github.com>
2022-12-01 10:44:11 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("Hello world! Thanks Deepak for testing me out");
         Student s = new Student();
         s.setI(2);
+        s.setI(5);
         System.out.println("Test branch 1"+s.getI());
     }
 }
